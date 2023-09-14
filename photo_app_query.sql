c706cb32ae2a389d12f248a5e78a728ec2f87956
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2023-09-14 13:02:14 -0600

    Include List of Addresses Into Response

---
+++ photo_app_query.sql
@@ -0,0 +1,12 @@
+
+use photo_app;
+show tables;
+select * from users;
+describe users;
+drop table users;
+drop table hibernate_sequence;
+create database photo_app;
+show tables;
+delete from users;
+select * from addresses;
+delete from addresses;
